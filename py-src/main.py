0c195c619b14bf11ca5a8dbe6d55065086145b5e
javier <javier@castroparga.com>
2021-05-19 23:06:45 +0200

    Initial css

--- py-src/main.py
+++ py-src/main.py
@@ -5,6 +5,17 @@ The main module for the damage counter
 MODULE_NAME = 'torg-basic-targeting'
 
 
+def modify_message(message, html):
+    """
+    Changes message adding a cross (targeting icon)
+    """
+    target_element = ' <i class="fas fa-crosshairs tbt-crosshair"></i>'
+    html.find('.skill-roll-result').append(target_element)
+    # TODO: Get the skill real skill name
+    # TODO: Call a function when the target_element is clicked, with the
+    # skill as a paramether
+
+
 def on_ready():
     """
     Foundry is ready
@@ -13,3 +24,4 @@ def on_ready():
 
 
 Hooks.once("ready", on_ready)  # noqa
+Hooks.on("renderChatMessage", modify_message)  # noqa
