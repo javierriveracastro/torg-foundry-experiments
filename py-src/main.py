9293fca958abc5960cdcb58fdc65212eb5d533b8
javier <javier@castroparga.com>
2021-05-28 20:15:57 +0200

    Show differences

--- py-src/main.py
+++ py-src/main.py
@@ -16,7 +16,7 @@ def get_token_or_actor_from_message(message):
         if token:
             actor = token.actor
     if not token:
-        actor = game.actors.js_get(memoryview.data.speaker.actor)  # noqa
+        actor = game.actors.js_get(message.data.speaker.actor)  # noqa
     return actor
 
 
@@ -33,14 +33,49 @@ def modify_message(message, html):
     """
     Changes message adding a cross (targeting icon)
     """
-    target_element = ' <i class="fas fa-crosshairs tbt-crosshair"></i>'
-    html.find('.skill-roll-result').append(target_element)
+    roll_result_html = html.find('.skill-roll-result')
+    try:
+        roll_result = int(roll_result_html.text())
+    except ValueError:
+        return
+    target_html = (' <i class="fas fa-crosshairs tbt-crosshair"></i>')
+    roll_result_html.append(target_html)
     # Get the skill real skill name
     chat_title = html.find(".skill-chat-title").text()
     skill = find_skill(get_token_or_actor_from_message(message),
                        chat_title[:-5])
-    console.log(skill)
     # TODO: Call a function when the target_element is clicked, with the
+
+    def show_defenses(event):
+        """
+        Shows the target defenses and its difference with the current roll
+        """
+        if not canvas:  # noqa
+            return
+        if canvas.tokens.controlled.lenght < 1:  # noqa
+            return
+        DEFENSES = ['dodge', 'meleeWeapons', 'unarmedCombat', 'intimidation',
+                    'maneuver', 'taunt', 'trick']
+        content = "<p>Resultado: {}</p>".format(roll_result)
+        for token in canvas.tokens.controlled:  # noqa
+            content += '<p>{}</p>'.format(token.actor.data.js_name)
+            skills = dict(token.actor.data.data.skills)
+            for defense in DEFENSES:
+                trans_skill = game.i18n.localize(  # noqa
+                    "torgeternity.skills." + defense)
+                current_skill = skills.get(defense)
+                content += '- {} ({}) = <strong>{}</strong><br>'.format(
+                    trans_skill, current_skill.value,
+                    roll_result - current_skill.value)
+
+        chatData = {'user': game.user._id,  # noqa
+                    'type': CONST.CHAT_MESSAGE_TYPES.OOC,  # noqa
+                    'blind': ChatMessage.getWhisperRecipients("GM"),  # noqa
+                    'speaker': ChatMessage.getSpeaker(),  # noqa
+                    'content': content}
+        ChatMessage.create(chatData)  # noqa
+
+    html.find('.tbt-crosshair').click(show_defenses)
     # skill as a paramether
 
 
